fab96d2ce3e7cdf1341bdb9dd959d09341819d7d
Davi Perdigão <davivcperdigao@gmail.com>
2021-08-31 09:27:11 -0300

    Aula 5

---
+++ Games 3D/Advanced I/Aula 5/EX2.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main (string[] args)
+    {
+        for (int i=2; i<=100; i=i +2)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
---
+++ Games 3D/Advanced I/EX1.cs
@@ -0,0 +1,43 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    string mes = Console.ReadLine();
+    if ( mes == "janeiro"){
+      Console.WriteLine("Tem 31 dias");
+    }
+    else if ( mes == "fevereiro"){
+      Console.WriteLine("Tem 28 dias");
+    }
+    else if ( mes == "março"){
+      Console.WriteLine("Tem 31 dias");
+    }
+    else if ( mes == "abril"){
+      Console.WriteLine("Tem 30 dias");
+    }
+    else if ( mes == "maio"){
+      Console.WriteLine("Tem 31 dias");
+    }
+    else if ( mes == "junho"){
+      Console.WriteLine("Tem 30 dias");
+    }
+    else if ( mes == "julho"){
+      Console.WriteLine("Tem 31 dias");
+    }
+    else if ( mes == "agosto"){
+      Console.WriteLine("Tem 31 dias");
+    }
+    else if ( mes == "setembro"){
+      Console.WriteLine("Tem 31 dias");
+    }
+    else if ( mes == "outubro"){
+      Console.WriteLine("Tem 31 dias");
+    }
+    else if ( mes == "novembro"){
+      Console.WriteLine("Tem 31 dias");
+    }
+    else if ( mes == "dezembro"){
+      Console.WriteLine("Tem 31 dias");
+    }
+  }
+}
